bed06d8e88f1cbd8ecb965bb3a01d02ce5d546f3
katiadobra <broun.kat@gmail.com>
2020-09-15 12:17:50 +0200

    Initial commit

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Calculator = props => {
+  return (
+    <div>
+      <form>
+        Calculator
+      </form>
+    </div>
+  )
+};
+
+export default Calculator;
