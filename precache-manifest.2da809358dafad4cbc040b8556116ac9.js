b32946dc3b36026fd169658020e6de476eecb0ee
katiadobra <broun.kat@gmail.com>
2020-09-24 15:22:07 +0200

    Updates

---
+++ precache-manifest.2da809358dafad4cbc040b8556116ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c06a8dca25177b712d042c751848b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b4692d970c94f29f641",
+    "url": "/static/css/main.ed6616a1.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4692d970c94f29f641",
+    "url": "/static/js/main.eb2b43ca.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
